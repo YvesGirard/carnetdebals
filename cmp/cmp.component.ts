ac02db50c862c8891a11b12868e645385f68720f
Yves <yv.girard@gmail.com>
2016-09-28 17:09:03 +0200

    heroes to rout-dashboard

--- cmp/cmp.component.ts
+++ cmp/cmp.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'cmp-app',
-  template: '<h1>My First Angular App</h1>'
+  template: `
+   <h1>{{title}}</h1>
+   <a routerLink="/heroes">Compétiteurs</a>
+   <router-outlet></router-outlet>
+ `
 })
-export class CmpComponent { }
+
+export class CmpComponent {
+  title = 'Liste des compétiteurs';
+}
+
--- cmp/cmp.module.ts
+++ cmp/cmp.module.ts
@@ -1,11 +1,27 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { routing } from './app.routing';
 
 import { CmpComponent }   from './cmp.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ CmpComponent ],
-  bootstrap:    [ CmpComponent ]
+  imports:      [ 
+    BrowserModule,
+    FormsModule,
+    routing 
+    ],
+  declarations: [ 
+    CmpComponent,
+    HeroesComponent,
+    HeroDetailComponent 
+    ],
+  bootstrap:    [ CmpComponent ],
+  providers: [HeroService]
 })
-export class CmpModule { }
+export class CmpModule { 
+  
+}
